e04daf5194e0cae06b85f3a138eb14275646acc1
Fidelcplusplus <iscfidel1a@gmail.com>
2025-04-23 22:50:00 -0600

    3.0. Validacion de incrementos elevados (Realizar pruebas)

--- src/main/java/com/stcm/Services/TorniqueteCountServices.java
+++ src/main/java/com/stcm/Services/TorniqueteCountServices.java
@@ -6,75 +6,81 @@ import com.stcm.Entity.TorniqueteCount;
 
 public class TorniqueteCountServices {
 	
-	
 	public void iterationRows(List<TorniqueteCount> data, int size_csv) {
 		TorniqueteCountServices torniqueteServices = new TorniqueteCountServices();
 		
-        for (int i = 0; i < size_csv -1; i++) {
-        	//Variable para la posicion actual y la posicion siguiente...
-        	TorniqueteCount value_act = data.get(i);
-        	TorniqueteCount value_sig = data.get(i+1);
-            TorniqueteCount value_ant = null;
-            long contador_ant=0; 
-            
-            // Solo obtenemos el valor anterior si no estamos en la primera posición
-            if (i > 0) {
-                value_ant = data.get(i-1);
-                contador_ant = value_ant.getContadorAuxiliar();
-            }
-        	
-        	//Obtiene el valor de las entradas actuales y entradas siguientes, a exceopcion de contador auxiliar...
-        	long entradas_act = value_act.getEntradas();
-        	long entradas_sig = value_sig.getEntradas();
-        	long contador_act = value_act.getContadorAuxiliar();
-        	
-            torniqueteServices.processValidation( entradas_act,  
-            		entradas_sig,  contador_act, contador_ant,  value_sig, value_act);
-        	
-		}
+	        for (int i = 0; i < size_csv -1; i++) {
+	        	//Variable para la posicion actual y la posicion siguiente...
+	        	TorniqueteCount value_act = data.get(i);
+	        	TorniqueteCount value_sig = data.get(i+1);
+	            TorniqueteCount value_ant = null;
+	            long contador_ant = 0;
+	            
+		            // Solo obtenemos el valor anterior si no estamos en la primera posición
+		            if (i > 0) {
+		                value_ant = data.get(i-1);
+		                contador_ant = value_ant.getContadorAuxiliar();
+		            }
+	        	
+	        	//Obtiene el valor de las entradas actuales y entradas siguientes, a exceopcion de contador auxiliar...
+	        	long entradas_act = value_act.getEntradas();
+	        	long entradas_sig = value_sig.getEntradas();
+	        	long contador_act = value_act.getContadorAuxiliar();
+	        	long real_value_act = value_act.getRealValue();
+	        	
+	            torniqueteServices.processValidation( entradas_act,  
+	            		entradas_sig,  contador_act, contador_ant,  value_sig, value_act,  real_value_act);
+	        	
+			}
 	}
 	
-	public void processValidation(long entradas_act, long entradas_sig, long contador_act, long contador_ant, TorniqueteCount value_sig, TorniqueteCount value_act) {
-		long var_aux;
+	private void processValidation(long entradas_act, long entradas_sig, long contador_act, long contador_ant, 
+		TorniqueteCount value_sig, TorniqueteCount value_act, long real_value_act) {
 		
-		//System.out.println("Actual: "+entradas_act+", Siguiente: "+entradas_sig+ ", Anterior: "+ contador_ant);
-		
-    	if(entradas_sig >= entradas_act && (entradas_sig - entradas_act) <= 50) {
-    		addAuxCount(entradas_act, entradas_sig, contador_act, value_sig, value_act, contador_ant);
-    		
-    	}else if(entradas_sig <= entradas_act) { 
-    		addAuxCount(entradas_act, entradas_sig, contador_act, value_sig, value_act, contador_ant);
-    		value_sig.setContadorAuxiliar(contador_act);
-    		
-    	}
+	    	if(entradas_sig >= entradas_act && (entradas_sig - entradas_act) < 51) {
+	    		addAuxCountMin(entradas_act, entradas_sig, contador_act, value_sig, value_act, contador_ant, real_value_act);
+	    		
+	    	}else if(entradas_sig <= entradas_act) { 
+	    		addAuxCountMin(entradas_act, entradas_sig, contador_act, value_sig, value_act, contador_ant, real_value_act);			
+	    		value_sig.setContadorAuxiliar(contador_act);
+	    		
+	    	}else if((entradas_sig-entradas_act) > 50) {
+	    		value_sig.setEntradas(entradas_act);
+	    		value_sig.setContadorAuxiliar(entradas_act);
+	    	}
     	
-    	//var_aux = entradas_act;
 	}
 	
-	public static void addAuxCount(long entradas_act, long entradas_sig, long contador_act, TorniqueteCount value_sig,TorniqueteCount value_act , long contador_ant) {
+	private  void addAuxCountMin(long entradas_act, long entradas_sig, long contador_act, TorniqueteCount value_sig,
+			TorniqueteCount value_act , long contador_ant, long real_value_act) {
 		
 			if(contador_act == 0) {
 				contador_act = contador_ant;
 				value_act.setContadorAuxiliar(contador_ant);
-			}
+			}  
+			
 			//Almacena la diferencia que hay entre el siguiente registro y el registro actual, para posteriormente
 			// ser sumado a la variable  actual.
-			long diferencia = contador_act + (entradas_sig - entradas_act);
+			long diferencia_cont = contador_act + (entradas_sig - entradas_act);
+			long contador_aux;
 			
 			//Se suma el valor de la posicion actual de la columna "ContadorAuxiliar"
-			long contador_aux = value_sig.getContadorAuxiliar() + diferencia;
-			
+			//contador_aux = value_sig.getContadorAuxiliar() + diferencia_cont;					
+			contador_aux = diferencia_cont;
 			//Almacena el valor en la posicion correspondiente.
 			value_sig.setContadorAuxiliar(contador_aux);
-			value_act.setEvaluar(9999);
 			
-			diferencia = 0;
-		
+			value_act.setEvaluar(9999);
+			diferencia_cont = 0;    
+			
 	}
 	
     public void mostrarTorniquetes(List<TorniqueteCount> list) {
-        for (TorniqueteCount item : list) {
-            System.out.println(item);
-        }
+    	int i = 0;
+	        for (TorniqueteCount item : list) {
+	            System.out.println("["+(i+1)+"]. "+item);
+	            i++;
+	        }
     }
+    
 }
